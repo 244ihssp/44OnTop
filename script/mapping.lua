d5caeed88952eda6b2789c19af5cd6f879b0c4d4
244ihssp <ihenne.aniworld@gmail.com>
2026-01-04 19:42:43 +0100

    Updated user mapping

--- script/mapping.lua
+++ script/mapping.lua
@@ -1,13 +1,16 @@
 --[[
     Discord User to Roblox UserID Mapping
     Auto-generated - DO NOT EDIT MANUALLY
-    Last updated: 2026-01-04 18:42:19.213338+00:00
+    Last updated: 2026-01-04 18:42:49.214951+00:00
 ]]
 
 -- Format: Discord UserID -> [Roblox UserIDs]
 
 -- Discord User: 1183551181985960038
 -- Roblox IDs: 1234, 12345
+
+-- Discord User: 1385890954350694521
+-- Roblox IDs: 123456
 
 
 -- JSON Data (for bot parsing):
@@ -16,6 +19,9 @@
     "1183551181985960038": [
         1234,
         12345
+    ],
+    "1385890954350694521": [
+        123456
     ]
 }
 ]]
