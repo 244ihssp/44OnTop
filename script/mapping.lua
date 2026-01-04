fcef2b9be8c834717090b6601b85d1ac0ba910e9
244ihssp <ihenne.aniworld@gmail.com>
2026-01-04 19:42:13 +0100

    Updated user mapping

--- script/mapping.lua
+++ script/mapping.lua
@@ -1,20 +1,21 @@
 --[[
     Discord User to Roblox UserID Mapping
     Auto-generated - DO NOT EDIT MANUALLY
-    Last updated: 2026-01-04 18:40:07.756440+00:00
+    Last updated: 2026-01-04 18:42:19.213338+00:00
 ]]
 
 -- Format: Discord UserID -> [Roblox UserIDs]
 
 -- Discord User: 1183551181985960038
--- Roblox IDs: 1234
+-- Roblox IDs: 1234, 12345
 
 
 -- JSON Data (for bot parsing):
 --[[
 {
     "1183551181985960038": [
-        1234
+        1234,
+        12345
     ]
 }
 ]]
